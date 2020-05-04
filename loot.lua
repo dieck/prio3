09088a70cce710e5496a685b82b17ac9a71eb564
Marco Dieckhoff <marco.dieckhoff@googlemail.com>
2020-05-04 08:41:40 +0200

    removed wrongly uploaded file, added pkgmeta for wowace libraries

--- loot.lua
+++ loot.lua
@@ -1,24 +1,6 @@
-
 function Prio3:handleChatCommand()
 	Prio3:guiPriorityFrame()
 end
-
-
-function Prio3:guiPriorityFrameDUMMY()
-	local f = CreateFrame("Frame",nil,UIParent)
-	f:SetFrameStrata("BACKGROUND")
-	f:SetWidth(820) -- Set these to whatever height/width is needed 
-	f:SetHeight(500) -- for your Texture
-
-	local t = f:CreateTexture(nil,"BACKGROUND")
-	t:SetTexture("Interface\\Glues\\CharacterCreate\\UI-CharacterCreate-Factions.blp")
-	t:SetAllPoints(f)
-	f.texture = t
-
-	f:SetPoint("CENTER",0,0)
-	f:Show()
-end
-
 
 function Prio3:guiPriorityFrame()
 	local AceGUI = LibStub("AceGUI-3.0")
