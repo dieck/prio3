3978d04b3fcb8cba24b42cb8ac54481cf653af80
Marco Dieckhoff <marco.dieckhoff@googlemail.com>
2021-01-17 10:30:01 +0100

    updated version

--- core.lua
+++ core.lua
@@ -1,7 +1,7 @@
 local L = LibStub("AceLocale-3.0"):GetLocale("Prio3", true)
 
 local Prio3commPrefix = "Prio3-1.0-"
-local Prio3versionString = "v20210102"
+local Prio3versionString = "v20210117"
 
 local defaults = {
   profile = {
